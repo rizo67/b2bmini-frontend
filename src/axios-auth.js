7107fb1c0f26d8e147b7877d4e1ce6e5a8be9f4a
rizo67 <riskutia.zoltan@gmail.com>
2020-12-20 19:41:15 +0100

    version_2020_1220

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000'
+  //baseURL: 'http://localhost:3000/dev'
+  baseURL: 'https://kdjsicjk6k.execute-api.eu-central-1.amazonaws.com/dev'
 })
 
 instance.defaults.headers.common['SOMETHING'] = 'something'
